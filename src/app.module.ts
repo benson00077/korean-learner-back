d0a5308c5d101f827ef0d61898f3663c99ea2d72
Benson <48788513+benson00077@users.noreply.github.com>
2022-10-30 22:30:08 +0800

    feat: init config module on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development', '.env.production'],
+      // isGlobal: true,
+      load: [configuration],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
+      validationOptions: {
+        allowUnknown: true, // nvm have env var like NVM_INC
+        abortEarly: true, // stops validation on the first error 
+      }
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
