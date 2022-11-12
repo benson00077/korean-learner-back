8b8d37f55e8b09e376a63b11d8f0e43d399ed0f9
Benson <48788513+benson00077@users.noreply.github.com>
2022-11-12 17:09:37 +0800

    test: config

--- src/sentence-ko/sentence-ko.controller.spec.ts
+++ src/sentence-ko/sentence-ko.controller.spec.ts
@@ -1,15 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Repository } from 'typeorm';
 import { SentenceKoController } from './sentence-ko.controller';
+import { SentenceKoService } from './sentence-ko.service';
+import { SentenceKo } from './sentenceKo.entity';
 
 describe('SentenceKoController', () => {
   let controller: SentenceKoController;
+  let service: SentenceKoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SentenceKoController],
+      providers: [
+        {
+          provide: SentenceKoService,
+          useClass: Repository<SentenceKo>,
+        },
+      ],
     }).compile();
 
     controller = module.get<SentenceKoController>(SentenceKoController);
+    service = module.get<SentenceKoService>(SentenceKoService);
   });
 
   it('should be defined', () => {
--- src/sentence-ko/sentence-ko.service.spec.ts
+++ src/sentence-ko/sentence-ko.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Repository } from 'typeorm';
 import { SentenceKoService } from './sentence-ko.service';
+import { SentenceKo } from './sentenceKo.entity';
 
 describe('SentenceKoService', () => {
   let service: SentenceKoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SentenceKoService],
+      providers: [
+        SentenceKoService,
+        {
+          provide: SentenceKoService,
+          useClass: Repository<SentenceKo>,
+        },
+      ],
     }).compile();
 
     service = module.get<SentenceKoService>(SentenceKoService);
